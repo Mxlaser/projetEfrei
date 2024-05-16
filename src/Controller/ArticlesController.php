158635710df3b903f6e231dede76e69d4f1af40c
niko <nikola.zivkovic300@gmail.com>
2024-05-16 14:52:34 +0200

    route article

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Articles;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
