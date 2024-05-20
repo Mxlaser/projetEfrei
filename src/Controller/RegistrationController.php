761b46912a45ae211cf05bcd00d5f47ed4d2c7bd
Mxlaser <nikola.zivkovic300@gmail.com>
2024-05-20 17:46:09 +0200

    nouveau pc

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Role;
 use App\Form\RegistrationFormType;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -80,7 +80,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         return (string) $this->email;
     }
-
+    
     /**
      * @see UserInterface
      */
