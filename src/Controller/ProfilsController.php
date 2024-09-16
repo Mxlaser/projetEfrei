0e0ec00d2b19bf72d2c0be42e904ad69091fc59c
niko <nikola.zivkovic300@gmail.com>
2024-09-16 14:36:54 +0200

    gestion users

--- src/Controller/ProfilsController.php
+++ src/Controller/ProfilsController.php
@@ -10,12 +10,44 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProfilsController extends AbstractController
 {
-    #[Route('/profils', name: 'app_profils')]
-    public function index(ManagerRegistry $doctrine): Response
+    #[Route('/profils/admin', name: 'profils_admin')]
+    public function admin(ManagerRegistry $doctrine): Response
     {
+        // Récupère tous les utilisateurs
         $users = $doctrine->getRepository(User::class)->findAll();
-        return $this->render('profils/profils.html.twig', [
+
+        return $this->render('profils/profils-admin.html.twig', [
             'user' => $users,
         ]);
+    }
+
+    #[Route('/profils/guest', name: 'profils_guest')]
+    public function guest(): Response
+    {
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('profils/profils-guest.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/profils/delete/{id}', name: 'profils_delete')]
+    public function delete(ManagerRegistry $doctrine, $id)
+    {
+        $user = $this->getUser();
+        if (!$user instanceof User || (!$user->hasRole('ADMIN')) && !$user->hasRole('USER')) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $user = $doctrine->getRepository(User::class)->find($id);
+        $entityManager = $doctrine->getManager();
+
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('profils_admin');
     }
 }
